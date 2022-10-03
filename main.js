0edfceeb6e35274bf15563f785e4b1fd1886dfab
omgbfgf <91060569+omgbfgf@users.noreply.github.com>
2022-10-03 16:56:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+canvas=createCanvas(333,333);
+canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save("jokerimage.jpg");
+}
